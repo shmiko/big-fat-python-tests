1bcb00efbeebbede81730bdf549614dabdc8bdcb
shmiko <shmiko@gmail.com>
2016-04-27 19:29:14 +1000

    more tests

--- logpuzzle/logpuzzle.py
+++ logpuzzle/logpuzzle.py
@@ -25,7 +25,7 @@ def read_urls(filename):
   Screens out duplicate urls and returns the urls sorted into
   increasing order."""
   # +++your code here+++
-  # wtf
+  
   
 
 def download_images(img_urls, dest_dir):

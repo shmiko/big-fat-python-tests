7d7ba8e32ddd046348f2cfdf9c7f61e91324eec5
shmiko <shmiko@gmail.com>
2016-05-02 17:29:44 +1000

    scrape lxml

---
+++ scrapes_lxml.py
@@ -0,0 +1,8 @@
+import sys
+#import pandas
+import pandas as pd
+#import the Beautiful soup functions to parse the data returned from the website
+from bs4 import BeautifulSoup
+from bs4 import SoupStrainer
+#import the library used to query a website
+import urllib2

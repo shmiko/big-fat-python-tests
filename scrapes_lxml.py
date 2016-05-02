d9b8816b9e36e29d8514e2aa6ab6de29017d6e36
shmiko <shmiko@gmail.com>
2016-05-02 18:08:20 +1000

    scrape itunes

--- scrapes_lxml.py
+++ scrapes_lxml.py
@@ -4,5 +4,45 @@ import pandas as pd
 #import the Beautiful soup functions to parse the data returned from the website
 from bs4 import BeautifulSoup
 from bs4 import SoupStrainer
+from lxml import html
+import requests
 #import the library used to query a website
 import urllib2
+
+class AppCrawler:
+  def __init__(self,starting_url,depth):
+    self.starting_url = starting_url
+    self.depth = depth
+    self.apps = []
+
+  def crawl(self):
+    self.get_app_from_link(self.starting_url)
+    return
+
+  def get_app_from_link(self,link):
+    start_page = requests.get(link)
+    
+    # print start_page.text
+    return
+
+
+class App:
+  def __init__(self,name,developer,price,links):
+    self.name = name
+    self.developer = developer
+    self.price = price
+    self.links = links
+
+  def __str__(self):
+    return ("Name: " + self.name.encode('UTF-8') 
+      + "\r\nDeveloper: " + self.developer.encode('UTF-8')
+      + "\r\nPrice: " + self.price.encode('UTF-8') 
+      + "\r\n")
+
+
+crawler = AppCrawler('https://itunes.apple.com/us/app/candy-crush-sage/id553834731',0)
+crawler.crawl()
+
+for app in crawler.apps:
+  print app
+

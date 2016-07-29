2284b880db1248ac079c4f1ee8b18b94f00e8d05
Paul Jones'git config --global user.email pjones@calmapit.comgit config --global user.name 'Paul Jones <pjones@calmapit.com>
2016-07-29 16:04:18 +1000

    fix mongodb

--- mongodb_query.py
+++ mongodb_query.py
@@ -6,3 +6,17 @@ db.schedule.find("bookings" : {"$elemMatch" : { "date" : new ISODate("2016-08-09
 			}
 		]
 	}
+	
+	db.schedule.insert(
+   { "bookings" : [
+			{
+				"event" : "MongoDB On Site Interveiw",
+				"date": ISODate("2016-08-09T10:00:00.000Z")
+			}
+		]
+	}
+)
+
+db.schedule.find()
+{ "_id" : ObjectId("579af1b356bf8dd7f454ae3e"), "bookings" : [ { "event" : "MongoDB On Site Interveiw", "date" : ISODate("2016-08-09T10:00:00Z") } ] }
+

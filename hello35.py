e90d7080147b865ce12d09d86516119378bdf89f
Paul Jones'git config --global user.email pjones@calmapit.comgit config --global user.name 'Paul Jones <pjones@calmapit.com>
2016-07-29 14:20:41 +1000

    fix 35

--- hello35.py
+++ hello35.py
@@ -21,11 +21,11 @@ import sys
 
 # # Define a main() function that prints a little greeting.
 def main():
-#   # Get the name from the command line, using 'World' as a fallback.
+  # Get the name from the command line, using 'World' as a fallback.
   if len(sys.argv) >= 2:
-    name = len(sys.argv[1])
+    name = sys.argv[1]
   else:
-    name = 'World'
+    name = 'Worldly'
   print ('Gday', name)
 
 # # This is the standard boilerplate that calls the main() function.

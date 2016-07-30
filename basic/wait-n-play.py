919a0ef7194411706a5c6380d06ec06c1df02ebc
shmiko <shmiko@gmail.com>
2016-07-30 16:49:48 +1000

    yello video

---
+++ basic/wait-n-play.py
@@ -0,0 +1,5 @@
+import webbrowser
+new = 2
+url = "https://www.google.com.au/url?sa=t&rct=j&q=&esrc=s&source=video&cd=7&cad=rja&uact=8&ved=0ahUKEwjM9J3txprOAhWFI5QKHbIVDNQQtwIIMDAG&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DD_ksYVM2cWY&usg=AFQjCNFmoTk2G33l_Ck51-c6h10YZ9bFmg&sig2=amJwdaDoO5_i5yd8otK3uQ&bvm=bv.128617741,d.dGo"
+webbrowser.open(url,new=2)
+

0bc4a8e3c39445a6b6f04d174adeac3d52ebe7e8
shmiko <shmiko@gmail.com>
2016-07-31 13:05:23 +1000

    split  str

--- profanity_filter.py
+++ profanity_filter.py
@@ -2,7 +2,16 @@ def read_text():
 	movies = open("curse_movies.txt")
 	contents_of_file = movies.read()
 	length_of_file = len(contents_of_file)
-	print ("length of movie string is " + length_of_file)
+	print (length_of_file)
 	print (contents_of_file)
 	movies.close()
+	find_curse_words(contents_of_file)
+
+def find_curse_words(text):
+	#split the text into an array
+	words = text.split(" ")
+	for word in words:
+		print (word)
+
+
 read_text()

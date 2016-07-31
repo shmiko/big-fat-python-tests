5cf478663798539bf93cfa9fce70c36ccd523de7
shmiko <shmiko@gmail.com>
2016-07-31 12:54:04 +1000

    len of str

--- profanity_filter.py
+++ profanity_filter.py
@@ -1,6 +1,8 @@
 def read_text():
 	movies = open("curse_movies.txt")
 	contents_of_file = movies.read()
+	length_of_file = contents_of_file.len()
+	print ("length of movie string is " + length_of_file)
 	print (contents_of_file)
 	movies.close()
 read_text()

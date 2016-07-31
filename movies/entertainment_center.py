1ba39076c24998cf9fb6e776cec14dcb28d9c5b3
shmiko <shmiko@gmail.com>
2016-07-31 16:50:52 +1000

    movies list

--- movies/entertainment_center.py
+++ movies/entertainment_center.py
@@ -1,5 +1,38 @@
 import media
 
-toy_story = media.Movie("Toy Story","A Boy's toys come to life","https://en.wikipedia.org/wiki/Toy_Story#/media/File:Toy_Story.jpg","https://www.google.com.au/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=0ahUKEwihr6HD-5zOAhVDFZQKHYCjCUoQ3ywIHjAA&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DKYz2wyBy3kc&usg=AFQjCNEJoSlxDJtzDTMi_7zEW-AOc2Z4MA&sig2=GF4roIebdb3G6PZWVXh88A")
+toy_story = media.Movie("Toy Story",
+						"A Boy's toys come to life",
+						"https://en.wikipedia.org/wiki/Toy_Story#/media/File:Toy_Story.jpg",
+						"www.youtube.com%2Fwatch%3Fv%3DKYz2wyBy3kc&usg=AFQjCNEJoSlxDJtzDTMi_7zEW-AOc2Z4MA&sig2=GF4roIebdb3G6PZWVXh88A")
 
-print (toy_story.storyline)
+print(toy_story.storyline)
+
+avatar = media.Movie("Avatar",
+						"A marine on an alien planet",
+						"https://en.wikipedia.org/wiki/avatar#/media/File:avatar.jpg",
+						"https://www.youtube.com/watch?v=cRdxXPV9GNQ")
+
+print(avatar.poster_image_url)
+avatar.show_trailer()
+
+suicide_sqad = media.Movie("Suicide Sqaud",
+						"DC Universe superhero filem",
+						"https://upload.wikimedia.org/wikipedia/en/thumb/5/50/Suicide_Squad_%28film%29_Poster.png/220px-Suicide_Squad_%28film%29_Poster.png",
+						"https://www.google.com.au/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=0ahUKEwj6rpmcjZ3OAhWDG5QKHRB2B-IQyCkIHzAA&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DCmRih_VtVAs&usg=AFQjCNHLRUXU5utAVVDy6048NDSED_K5ZQ&sig2=Z5nSOM2E_QIUZ75N6YyiLg")
+
+ghostbusters = media.Movie("Ghostbusters 2016",
+						"ghosts run wild in New York City",
+						"https://upload.wikimedia.org/wikipedia/en/thumb/d/d0/Ghostbusters_2016_film_poster.jpg/220px-Ghostbusters_2016_film_poster.jpg",
+						"https://www.google.com.au/url?sa=t&rct=j&q=&esrc=s&source=web&cd=9&cad=rja&uact=8&ved=0ahUKEwjixcvVjJ3OAhXGHZQKHZSNA58QtwIIwQEwCA&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3Dw3ugHP-yZXw&usg=AFQjCNFYJJsOX2JMFDHmgGwmYW2N6m30-w&sig2=_ZWxpHYPUH8u7yvoRM_5YQ&bvm=bv.128617741,d.dGo")
+
+
+
+bfg = media.Movie("BFG",
+						"A big friendly giant, by Roald Dahl",
+						"https://upload.wikimedia.org/wikipedia/en/thumb/a/af/The_BFG_poster.jpg/220px-The_BFG_poster.jpg",
+						"https://www.google.com.au/url?sa=t&rct=j&q=&esrc=s&source=web&cd=14&cad=rja&uact=8&ved=0ahUKEwjsoJXBjZ3OAhUBwJQKHd9TAD0QtwII0QEwDQ&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DGZ0Bey4YUGI&usg=AFQjCNFhKabop0VtA8tQiyfmpw5WxkwNQg&sig2=77wA8xCpeX5kXCmMA70W9A&bvm=bv.128617741,d.dGo")
+
+independence_day = media.Movie("Independence Day - Resurgence",
+						"SciFi base on same film from 20 years ago, invasion",
+						"https://upload.wikimedia.org/wikipedia/en/thumb/5/58/Independence-Day-2-poster.jpg/220px-Independence-Day-2-poster.jpg",
+						"https://www.google.com.au/url?sa=t&rct=j&q=&esrc=s&source=web&cd=7&cad=rja&uact=8&ved=0ahUKEwjZvp71jZ3OAhVFJZQKHeFPBdwQtwIIwwEwBg&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DRfJgT89hEME&usg=AFQjCNGPQs4uSAgR6kcbSRaiEj93XL6DLw&sig2=P5llW5sYLmFOmXJ0DIQqgQ&bvm=bv.128617741,d.dGo")

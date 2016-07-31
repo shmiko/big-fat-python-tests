4bf60f3d3038c5266b3f5c3acdeeba3c20431ff2
shmiko <shmiko@gmail.com>
2016-07-31 13:12:51 +1000

    open browser wdyl

--- profanity_filter.py
+++ profanity_filter.py
@@ -1,3 +1,6 @@
+import os
+url_direct = "https://www.youtube.com/watch?q="
+
 def read_text():
 	movies = open("curse_movies.txt")
 	contents_of_file = movies.read()
@@ -10,8 +13,12 @@ def read_text():
 def find_curse_words(text):
 	#split the text into an array
 	words = text.split(" ")
+	count_of_words = 0
 	for word in words:
+		count_of_words = count_of_words + 1
+		if (webbrowser.open(url_direct + word,new=2)) == true:
+			os.rename(word,word.replace("curse!"))
 		print (word)
-
+	print (count_of_words)
 
 read_text()

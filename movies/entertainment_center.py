98746f077e9a9c28047120bccd76d264332f22ca
shmiko <shmiko@gmail.com>
2016-07-31 15:27:32 +1000

    movies

---
+++ movies/entertainment_center.py
@@ -0,0 +1,5 @@
+import media
+
+toy_story = media.Movie("Toy Story","A Boy's toys come to life","https://en.wikipedia.org/wiki/Toy_Story#/media/File:Toy_Story.jpg","https://www.google.com.au/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=0ahUKEwihr6HD-5zOAhVDFZQKHYCjCUoQ3ywIHjAA&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DKYz2wyBy3kc&usg=AFQjCNEJoSlxDJtzDTMi_7zEW-AOc2Z4MA&sig2=GF4roIebdb3G6PZWVXh88A")
+
+print (toy_story.storyline)
---
+++ movies/media.py
@@ -0,0 +1,6 @@
+class Movie():
+	def __init__(self,title,storyline,poster_image_url,trailer_youtube_url):
+		self.title = movie_title
+		self.storyline = movie_storyline
+		self.poster_image_url = poster_image
+		self.trailer_youtube_url = trailer_youtube

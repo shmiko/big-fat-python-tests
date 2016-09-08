6bc65164b925370879748ac90c2b97b5a3c57e9d
Paul Jones'git config --global user.email pjones@calmapit.comgit config --global user.name 'Paul Jones <pjones@calmapit.com>
2016-09-08 11:40:05 +1000

    find file string

--- find_file_string.py
+++ find_file_string.py
@@ -1,9 +1,9 @@
 import os
 
-for fname in os.listdir('.'):    # change directory as needed
+for fname in os.listdir('H:\Prism Report Exports\myCVS'):    # change directory as needed
     if os.path.isfile(fname):    # make sure it's a file, not a directory entry
         with open(fname) as f:   # open file
             for line in f:       # process line by line
-                if 'apples' in line:    # search for string
+                if 'kmart' in line:    # search for string
                     print 'found string in file %s' %fname
                     break
